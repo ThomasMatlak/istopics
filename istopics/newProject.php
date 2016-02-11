2f61f5397412628b61dc1145d07845942e9b7b35
tmatlak <tmatlak18@wooster.edu>
2016-02-11 10:35:22 -0500

    can add entries to the database through web form

--- istopics/newProject.php
+++ istopics/newProject.php
@@ -4,5 +4,30 @@
 * 
 * Add a new project to the istopics database.
 */
-?>
 
+$servername = "localhost";
+$username = "istopics";
+$password = "password"; //NOTE: CHANGE THE PASSWORD BEFORE GOING INTO PRODUCTION
+$dbname = "istopics";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$title = $_POST["title"];
+$abstract = $_POST["abstract"];
+$keywords = $_POST["keywords"];
+$comments = $_POST["comments"];
+
+$stmt = $conn->prepare("INSERT INTO projects (title, abstract, keywords, comments) VALUES (?, ?, ?, ?)");
+$stmt->bind_param("ssss", $title, $abstract, $keywords, $comments);
+
+$stmt->execute();
+
+$stmt->close();
+$conn->close();
+?>
