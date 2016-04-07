faa6a95ea87f0469b3f136a472ff31a4ec1e9315
tmatlak <tmatlak18@wooster.edu>
2016-04-07 12:50:24 -0400

    autofill only the correct major

--- istopics/js/setMajor.js
+++ istopics/js/setMajor.js
@@ -1,20 +1,21 @@
 // Set the major in the select multiple input
-$(document).ready(function() {
+$(document).ready(function () {
     if (document.getElementById('discipline') == null) { return 0; }
 
     var majors = document.getElementById('discipline').options;
 
     var student_majors = [];
 
-    var st_major = $('#st_major').val();
-    
+    var st_majors = $('#st_major').val().split(",");
+
     for (i = 0; i < majors.length; i++) {
-	if ($('#st_major').val().indexOf(majors[i].value) != -1) {
-	    student_majors.push(majors[i].value);
-	    $('#discipline_check').attr("class", "has-success");
+	for (j = 0; j < st_majors.length; j++) {
+	    if (st_majors[j] == majors[i].value) {
+		student_majors.push(majors[i].value);
+		$('#discipline_check').attr("class", "has-success");
+	    }
 	}
     }
-    
     $('#discipline').val(student_majors);
     $('#stud_major').text($('#discipline').val());
 });
