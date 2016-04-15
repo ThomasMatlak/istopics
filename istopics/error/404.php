810f464a238d26f924c26e9be66756c558886ae9
tmatlak <tmatlak18@wooster.edu>
2016-04-15 15:46:47 -0400

    added custom error pages

---
+++ istopics/error/404.php
@@ -0,0 +1,18 @@
+<?php
+/*
+* 404.php
+*
+* Error 404 page
+*/
+
+include '../header.php';
+
+echo <<<EOT
+    <h2>The Requested Content Could Not Be Found</h2>
+
+    <a href='/project/all' class='btn btn-class'>Return to Safety</a>
+EOT;
+
+include '../footer.php';
+
+?>
---
+++ istopics/error/500.php
@@ -0,0 +1,20 @@
+<?php
+/*
+* 500.php
+*
+* Error 500 page
+*/
+
+include '../header.php';
+
+echo <<<EOT
+    <h2></h2>
+
+    <a href='https://www.youtube.com/watch?v=4Xca6Y9eXmA?start=0s&end=6s&version=3'>I Think I've Broken Something</a>
+
+    <a href='/project/all' class='btn btn-class'>Return to Safety</a>
+EOT;
+
+include '../footer.php';
+
+?>
