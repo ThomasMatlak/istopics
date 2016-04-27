7a83bc1c2a1f9df00306dfdbd30651ee12a90796
Stephen Flynn <sflynn@wooster.edu>
2016-04-27 17:00:08 -0400

    Added 403 document for when people are denied access due to IP address

---
+++ istopics/error/403.php
@@ -0,0 +1,18 @@
+<?php
+/*
+* 403.php
+*
+* Error 403 page
+*/
+
+include '../header.php';
+
+echo <<<EOT
+    <h2>You must be connected to the College of Wooster network to access that page.</h2>
+
+    <a href='/project/all' class='btn btn-class'>Return to Safety</a>
+EOT;
+
+include '../footer.php';
+
+?>
