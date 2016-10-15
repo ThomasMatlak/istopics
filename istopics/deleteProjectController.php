42ced06e6cef9e71ee830f8c66360964b44af3a7
Thomas Matlak <tmatlak18@wooster.edu>
2016-10-15 12:41:28 -0400

    can delete projects using Project class

--- istopics/deleteProjectController.php
+++ istopics/deleteProjectController.php
@@ -9,6 +9,7 @@ if (!isset($_SESSION)) {session_start();}
 
 require_once 'db_credentials.php';
 require_once 'checkSignIn.php';
+require_once 'project.class.php';
 
 $id = filter_var($_POST["project_id"], FILTER_SANITIZE_STRING);
 
@@ -34,21 +35,9 @@ if (issignedin() != -1) {
      	exit();
     }
 
-    // Prepare the SQL statement
-    $stmt = $conn->prepare("DELETE FROM projects WHERE id=?");
-    $stmt->bind_param("s", $id);
+    $project = new Project();
+    $project->delete($id, $conn);
 
-    // Submit the SQL statement
-    $stmt->execute();
-    $stmt->close();
-
-    // Remove the connection between the user and the project
-    $stmt = $conn->prepare("DELETE FROM user_project_connections WHERE projectid=?");
-    $stmt->bind_param("s", $id);
-
-    $stmt->execute();
-
-    $stmt->close();
     $conn->close();
 
     $_SESSION["message"] = 2;
