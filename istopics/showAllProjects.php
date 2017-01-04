e67a3aa97b6cf3e56a6902eb96f3d47935608fcc
tmatlak <tmatlak18@wooster.edu>
2017-01-04 11:55:12 -0500

    added buttons to switch between tabular and list views

--- istopics/showAllProjects.php
+++ istopics/showAllProjects.php
@@ -64,6 +64,10 @@ $result = $conn->query($sql);
 // Display Projects
 if ($result->num_rows > 0) {
 ?>
+<ul class="nav nav-tabs">
+    <li <?php echo ($view == 'list') ? 'class="active"' : '' ?>><a <?php echo ($view == 'list') ? '' : 'href="./all"';?>>List</a></li>
+    <li <?php echo ($view == 'tabular') ? 'class="active"' : '' ?>><a <?php echo ($view == 'tabular') ? '' : 'href="./all?view=tabular"';?>>Tabular</a></li>
+</ul>
 <form class='form-inline'>
     <div class='form-group'>
         <input type='text' name='search' id='search' placeholder='Search Projects' class='form-control'>
